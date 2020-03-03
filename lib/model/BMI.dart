6aeb26653df45412d85abcb77ac74d9f6abd1454
mshayanabbasi <mshayanabbasi98@gmail.com>
2020-03-03 23:42:35 +0500

    done

---
+++ lib/model/BMI.dart
@@ -0,0 +1,10 @@
+class BMI {
+  double bmi;
+  bool isNormal;
+  String comments;
+  BMI(double bmi, bool isNormal, String comments) {
+    this.bmi = bmi;
+    this.isNormal = isNormal;
+    this.comments = comments;
+  }
+}
--- lib/screens/BMICalculatorScreen.dart
+++ lib/screens/BMICalculatorScreen.dart
@@ -1,5 +1,7 @@
+import 'package:bmi_app/screens/ResultScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import '../model/BMI.dart';
 
 class BMICalculatorScreen extends StatefulWidget {
   BMICalculatorScreen({Key key}) : super(key: key);
@@ -9,19 +11,155 @@ class BMICalculatorScreen extends StatefulWidget {
 }
 
 class _BMICalculatorScreenState extends State<BMICalculatorScreen> {
+  double _heightOfUser = 100.0;
+  double _weightOfUser = 40.0;
+  double _bmi = 0;
+  BMI _bmiModel;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              width: 200,
-              height: 200,
-              child: SvgPicture.asset("/assets/images/heart.svg", fit: BoxFit.contain,),
-            ),
-          ],
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.all(40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 200,
+                height: 200,
+                child: SvgPicture.asset(
+                  "assets/images/heart.svg",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                "BMI Calculator",
+                style: TextStyle(
+                  color: Colors.red[700],
+                  fontSize: 34,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              Text(
+                "We Care about your health",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Text(
+                "Height (cm)",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Slider(
+                  min: 80.0,
+                  max: 250.0,
+                  value: _heightOfUser,
+                  onChanged: (height) {
+                    setState(() {
+                      _heightOfUser = height;
+                    });
+                  },
+                  divisions: 100,
+                  activeColor: Colors.red,
+                  label: "$_heightOfUser",
+                ),
+              ),
+              Text(
+                "$_heightOfUser cm",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              Text(
+                "Weight (kg)",
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: Slider(
+                  min: 30.0,
+                  max: 120.0,
+                  value: _weightOfUser,
+                  onChanged: (weight) {
+                    setState(() {
+                      _weightOfUser = weight;
+                    });
+                  },
+                  divisions: 100,
+                  activeColor: Colors.red,
+                  label: "$_weightOfUser",
+                ),
+              ),
+              Text(
+                "$_weightOfUser kg",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w900,
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.only(left: 16, right: 16),
+                child: FlatButton.icon(
+                  onPressed: () {
+                    setState(() {
+                      _bmi = _weightOfUser /
+                          ((_heightOfUser / 100) * (_heightOfUser / 100));
+                      if (_bmi >= 18.5 && _bmi <= 25) {
+                        _bmiModel = BMI(_bmi, true, "You are already fit");
+                      } else if (_bmi < 18.5) {
+                        _bmiModel = BMI(_bmi, false, "You are Underweighted");
+                      } else if (_bmi > 25 && _bmi <= 30) {
+                        _bmiModel = BMI(_bmi, false, "You are Overweighted");
+                      } else {
+                        _bmiModel = BMI(_bmi, false, "You are Obesed");
+                      }
+                    });
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ResultScreen(
+                          bmiModel: _bmiModel,
+                        ),
+                      ),
+                    );
+                  },
+                  icon: Icon(
+                    Icons.favorite,
+                    color: Colors.white,
+                  ),
+                  label: Text("CALCULATE"),
+                  color: Colors.pink,
+                  textColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
